41037956fd6ea4c80df91af6a8888f3a43fe8c5a
winvex <winvex@DESKTOP-H4VED5I>
2024-04-07 11:38:39 +0300

    Add project files.

---
+++ LeaveManagement.Web/Data/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Identity.Client;
+
+namespace LeaveManagement.Web.Data
+{
+    public partial class BaseEntity // Using partial so BaseEntity cannot be instantiated by itself
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public int Salary {  get; set; }
+    }
+}
